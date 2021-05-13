0a554d4e7cf7b000266c5a70cb8c37777b274826
Berbet16 <63414835+Berbet16@users.noreply.github.com>
2021-05-13 23:36:49 +0300

    Step - 6

---
+++ MvcCv/Controllers/DeneyimController.cs
@@ -0,0 +1,37 @@
+﻿using MvcCv.Models.Entity;
+using MvcCv.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcCv.Controllers
+{
+    public class DeneyimController : Controller
+    {
+        // GET: Deneyim
+        DeneyimRepository repo = new DeneyimRepository();
+
+        public ActionResult Index()
+        {
+            //değerleri listeleme ve döndürme
+            var degerler = repo.List();
+            return View(degerler);
+        }
+
+        [HttpGet]
+        public ActionResult DeneyimEkle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult DeneyimEkle(TblDeneyimlerim p)
+        {
+            //parametreden gelen değeri ekle ve indexe yönlendir
+            repo.TAdd(p);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MvcCv/Repositories/GenericRepository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MvcCv.Models.Entity;
+
+namespace MvcCv.Repositories
+{
+    public class GenericRepository<T> where T : class, new()
+    {
+        DbCvEntities db = new DbCvEntities();
+
+        public List<T> List()
+        {
+            //t den gelen değeri liste olarak döndür.
+            //T =  tüm tabloların değeri
+            return db.Set<T>().ToList();
+        }
+
+        public void TAdd(T p)
+        {
+            //parametreden gelen değerleri ekle ve kaydet
+            db.Set<T>().Add(p);
+            db.SaveChanges();
+        }
+
+        public void TDelete(T p)
+        {
+            db.Set<T>().Remove(p);
+            db.SaveChanges();
+        }
+
+        public T TGet(int id)
+        {
+            //T den gelen değeri id 'e göre bul
+            return db.Set<T>().Find(id);
+        }
+
+        public void TUpdate(T p)
+        {
+            db.SaveChanges();
+        }
+    }
+}
