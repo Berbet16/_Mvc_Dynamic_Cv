2ba0d7de6bd3428b8415e1baab547667039e6d1a
Berbet16 <63414835+Berbet16@users.noreply.github.com>
2021-05-13 16:43:40 +0300

    Add project files.

---
+++ MvcCv/Controllers/DefaultController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCv.Models.Entity;
+
+namespace MvcCv.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        DbCvEntities db = new DbCvEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TblHakkimda.ToList();
+            return View(degerler);
+        }
+    }
+}
