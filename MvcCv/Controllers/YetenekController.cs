16f8b83bf8653940c52723c7abb738958deb50ab
Berbet16 <63414835+Berbet16@users.noreply.github.com>
2021-05-16 22:14:35 +0300

    Admin Paneli CRUD İşlemleri

--- MvcCv/Controllers/YetenekController.cs
+++ MvcCv/Controllers/YetenekController.cs
@@ -17,5 +17,44 @@ namespace MvcCv.Controllers
             var yetenekler = repo.List();
             return View(yetenekler);
         }
+
+        [HttpGet]
+        public ActionResult YeniYetenek()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult YeniYetenek(TblYeteneklerim p)
+        {
+            //repodan gelen değerleri ekle
+            repo.TAdd(p);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult YetenekSil(int id)
+        {
+            var yetenek = repo.Find(x => x.ID == id);
+            repo.TDelete(yetenek);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult YetenekDuzenle(int id)
+        {
+            var yetenek = repo.Find(x => x.ID == id);
+            return View(yetenek);
+        }
+
+        [HttpPost]
+        public ActionResult YetenekDuzenle(TblYeteneklerim t)
+        {
+            var y= repo.Find(x => x.ID == t.ID);
+            y.Yetenek = t.Yetenek;
+            y.Oran = t.Oran;
+            repo.TUpdate(y);//y den gelen değerleri güncelleyeceğiz
+            return RedirectToAction("Index");
+        }
+
     }
 }
--- MvcCv/Models/Entity/TblYeteneklerim.cs
+++ MvcCv/Models/Entity/TblYeteneklerim.cs
@@ -16,5 +16,6 @@ namespace MvcCv.Models.Entity
     {
         public int ID { get; set; }
         public string Yetenek { get; set; }
+        public Nullable<byte> Oran { get; set; }
     }
 }
