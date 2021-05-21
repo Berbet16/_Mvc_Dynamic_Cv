0e303b4022bf2918101d8a72cd71554af9db7b7b
Berbet16 <63414835+Berbet16@users.noreply.github.com>
2021-05-21 15:45:00 +0300

    Eğitim İşlemleri

--- MvcCv/Models/Entity/TblEgitimlerim.cs
+++ MvcCv/Models/Entity/TblEgitimlerim.cs
@@ -11,13 +11,17 @@ namespace MvcCv.Models.Entity
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class TblEgitimlerim
     {
         public int ID { get; set; }
+        [Required(ErrorMessage ="Bu Alaný Boþ Geçemezsiniz")]
         public string Baslik { get; set; }
         public string AltBaslik1 { get; set; }
         public string AltBaslik2 { get; set; }
+
+        [StringLength(10, ErrorMessage ="Lütfen en fazla 10 karakterlik veri giriþi yapýnýz")]
         public string GNO { get; set; }
         public string Tarih { get; set; }
     }
