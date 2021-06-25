f91f4376c788e726bd3a803d40df6d9ea426a338
Berbet16 <63414835+Berbet16@users.noreply.github.com>
2021-06-25 11:31:17 +0300

    23 / 06 / 2021

---
+++ MvcCv/Controllers/HobiController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCv.Models.Entity;
+using MvcCv.Repositories;
+
+namespace MvcCv.Controllers
+{
+    public class HobiController : Controller
+    {
+        // GET: Hobi
+        GenericRepository<TblHobilerim> repo = new GenericRepository<TblHobilerim>();
+        public ActionResult Index()
+        {
+            var hobiler = repo.List();
+            return View(hobiler);
+        }
+    }
+}
---
+++ MvcCv/Controllers/iletisimController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCv.Models.Entity;
+using MvcCv.Repositories;
+
+namespace MvcCv.Controllers
+{
+    public class iletisimController : Controller
+    {
+        // GET: iletisim
+        GenericRepository<Tbliletisim> repo = new GenericRepository<Tbliletisim>();
+
+        public ActionResult Index()
+        {
+            var mesajlar = repo.List();
+            return View(mesajlar);
+        }
+    }
+}
